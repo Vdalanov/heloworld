44a90e359e887f330f4bb402e718cdabd1ef8f78
Vladislav <vdalanov@yandex.ru>
2022-10-02 21:23:37 +0300

    Examples005

---
+++ examples005/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "влад")
+{
+    Console.WriteLine ("Ура это же Влад");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
+            
