2a4bd6dcbf2d1c0665d1921c161691fc33261f2e
Vladislav <vdalanov@yandex.ru>
2022-10-02 21:36:46 +0300

    Example006

---
+++ Example006/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = 1;
+int b = 4;
+int c = 6;
+int d = 8;
+int e = 7;
+
+int max=a;
+if (b>max) max=b;
+if (c>max) max=c;
+if (d>max) max=d;
+if (e>max) max=e;
+Console.Write("Самая тяжолая гиря= ");
+Console.WriteLine(max);
